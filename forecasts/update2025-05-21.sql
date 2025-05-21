f783411e393c000827d5369e64de50faf5618c24
steven <steven.hentschel@gmail.com>
2025-05-21 11:13:41 +0100

    corrected update statement

--- forecasts/update2025-05-21.sql
+++ forecasts/update2025-05-21.sql
@@ -1,1 +1,1 @@
-UPDATE INTO forecasts (starttime, sky) VALUES ('2025-05-21T11:00 Europe/London', 'Overcast');
+UPDATE forecasts SET sky = 'Overcast' WHERE starttime = '2025-05-21T11:00 Europe/London';
